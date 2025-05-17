7b257396afa4aa71e282e1542bed7a218c671479
sohail tanveer <sohailtanveer769@gmail.com>
2025-05-16 23:39:14 -0500

    corrected error for skills

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
-import Skills from "@/components/skills";
 
 const Home = () => {
   return (
